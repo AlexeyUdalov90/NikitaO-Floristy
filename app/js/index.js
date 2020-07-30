ea007f458970de410810ada05dd81fee76a91e97
Nikita <dedsec1612@gmail.com>
2020-07-30 07:53:21 +0300

    меню готово

--- app/js/index.js
+++ app/js/index.js
@@ -1,1 +1,9 @@
-console.log('Test js!!!');
+let menuBurger = document.querySelector('.header__menu-burger');
+let header = document.querySelector('.header');
+let mainMenu = document.querySelector('.main-menu');
+
+menuBurger.onclick = function () {
+  header.classList.toggle('header--active');
+  mainMenu.classList.toggle('hidden');
+}
+
