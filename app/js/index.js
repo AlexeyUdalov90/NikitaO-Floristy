472b865dd99c9bed669d3b90b353082cfae9d128
Nikita <dedsec1612@gmail.com>
2020-08-07 05:36:56 +0300

    весь JS в 1 файле

--- app/js/index.js
+++ app/js/index.js
@@ -1,7 +1,17 @@
-let orderButton = document.querySelector('.promo__button');
+let menuBurger = document.querySelector('.header__menu-burger');
+let header = document.querySelector('.header');
+let mainMenu = document.querySelector('.main-menu');
+let orderButton = document.querySelector('.button-js');
 let popup = document.querySelector('.callback-popup');
 let closeButton = document.querySelector('.callback__close-button');
 
+/* Menu-burger */
+menuBurger.onclick = function () {
+  header.classList.toggle('header--active');
+  mainMenu.classList.toggle('hidden');
+}
+
+/* Popup */
 orderButton.onclick = function () {
   popup.classList.remove('hidden');
   document.body.style.backgroundColor = "rgba(6, 6, 6, 0.5)";
