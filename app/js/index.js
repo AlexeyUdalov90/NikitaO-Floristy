c2fbbbf064ddf4542625a019d05676672e611e7f
Nikita <dedsec1612@gmail.com>
2020-08-07 07:41:25 +0300

    доработка попапа и менюшшки

--- app/js/index.js
+++ app/js/index.js
@@ -2,23 +2,29 @@ let menuBurger = document.querySelector('.header__menu-burger');
 let header = document.querySelector('.header');
 let mainMenu = document.querySelector('.main-menu');
 let orderButton = document.querySelector('.button-js');
-let popup = document.querySelector('.callback-popup');
+let popup = document.querySelector('.callback-popup-wrapper');
 let closeButton = document.querySelector('.callback__close-button');
 
 /* Menu-burger */
 menuBurger.onclick = function () {
   header.classList.toggle('header--active');
-  mainMenu.classList.toggle('hidden');
+  mainMenu.classList.toggle('main-menu--active');
+  if (header.classList.contains('header--active')) {
+    document.body.style.overflow = "hidden";
+  } else {
+    document.body.style.overflow = "visible";
+  }
 }
 
 /* Popup */
 orderButton.onclick = function () {
-  popup.classList.remove('hidden');
-  document.body.style.backgroundColor = "rgba(6, 6, 6, 0.5)";
+  popup.classList.add('callback-popup-wrapper--active');
+  document.body.style.overflow = "hidden";
+  
 }
 
 closeButton.onclick = function (evt) {
   evt.preventDefault();
-  popup.classList.add('hidden');
-  document.body.style.backgroundColor = "#3B181E";
+  popup.classList.remove('callback-popup-wrapper--active');
+  document.body.style.overflow = "visible";
 }
